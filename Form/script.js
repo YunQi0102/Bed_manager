921200c8c84a71661e743e83b7da17accaa640d7
mei970073 <93147911+YunQi0102@users.noreply.github.com>
2023-11-21 01:35:30 +0800

    Add RegisterForm & navbar

--- Form/script.js
+++ Form/script.js
@@ -1,7 +1,8 @@
 var Today = new Date();
 var day  = Today.getDay();
 var day_list = ['日', '一', '二', '三', '四', '五', '六'];
-document.querySelector(".current_time").innerHTML = Today.getFullYear()+ "年" + (Today.getMonth()+1) + "月" + Today.getDate() + "日" + ' 星期' + day_list[day] + " " + Today.getHours() + " : " + Today.getMinutes();
+document.querySelector(".current_time").innerHTML = Today.getFullYear() + "年" + (Today.getMonth()+1) + "月" + Today.getDate() + "日" + ' 星期' + day_list[day] + " " + Today.getHours() + " : " + Today.getMinutes();
+document.querySelector(".register_time").innerHTML = Today.getFullYear() + "/" + (Today.getMonth()+1) + "/" + Today.getDate();
 document.querySelector(".update_date").innerHTML =(Today.getMonth()+1) + "/" + Today.getDate();
 document.querySelector(".update_time").innerHTML =(Today.getHours()) + ":" + Today.getMinutes();
 
@@ -10,3 +11,4 @@ clickbtn.addEventListener('click', LogIn);
 function LogIn() {
     alert("登記成功!");
 };
+
