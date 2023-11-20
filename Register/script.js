1f8665b4cab61d1b1d8475d630bb45c0fdcab950
mei970073 <93147911+YunQi0102@users.noreply.github.com>
2023-11-20 11:57:46 +0800

    Add files via upload

---
+++ Register/script.js
@@ -0,0 +1,10 @@
+var Today = new Date();
+var day  = Today.getDay();
+var day_list = ['日', '一', '二', '三', '四', '五', '六'];
+document.querySelector(".current_time").innerHTML = Today.getFullYear()+ "年" + (Today.getMonth()+1) + "月" + Today.getDate() + "日" + ' 星期' + day_list[day] + " " + Today.getHours() + " : " + Today.getMinutes();
+
+const clickbtn = document.querySelector(".btn");
+clickbtn.addEventListener('click', LogIn);
+function LogIn() {
+    alert("登記成功!");
+};
