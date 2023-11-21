26f0c1485326a2f2433cd87d9368bccfbe37f5fb
mei970073 <93147911+YunQi0102@users.noreply.github.com>
2023-11-21 22:17:33 +0800

    navbar image & instant updates

--- Register/script.js
+++ Register/script.js
@@ -1,9 +1,19 @@
 var Today = new Date();
+var Year = Today.getFullYear();
+var Month = Today.getMonth();
+var Date = Today.getDate();
+var Hour = Today.getHours();
+var Minute = Today.getMinutes();
 var day  = Today.getDay();
 var day_list = ['日', '一', '二', '三', '四', '五', '六'];
-document.querySelector(".current_time").innerHTML = Today.getFullYear()+ "年" + (Today.getMonth()+1) + "月" + Today.getDate() + "日" + ' 星期' + day_list[day] + " " + Today.getHours() + " : " + Today.getMinutes();
-document.querySelector(".update_date").innerHTML =(Today.getMonth()+1) + "/" + Today.getDate();
-document.querySelector(".update_time").innerHTML =(Today.getHours()) + ":" + Today.getMinutes();
+
+document.querySelector(".update_date").innerHTML = (Month + 1) + "/" + Date;
+document.querySelector(".update_time").innerHTML = Hour + ":" + Minute;
+
+function current() {
+    document.querySelector(".current_time").innerHTML = Year + "年" + (Month + 1) + "月" + Date + "日" + ' 星期' + day_list[day] + " " + Hour + " : " + Minute;
+    setTimeout('current()', 60000);
+};
 
 const clickbtn = document.querySelector(".btn");
 clickbtn.addEventListener('click', LogIn);
