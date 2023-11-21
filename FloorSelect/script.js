a588c583b2dabd32665426dd2936b4620d3d04cb
mei970073 <93147911+YunQi0102@users.noreply.github.com>
2023-11-22 01:15:56 +0800

    navbar & instant update 2

--- FloorSelect/script.js
+++ FloorSelect/script.js
@@ -1,7 +1,26 @@
 var Today = new Date();
+var Year = Today.getFullYear();
+var Month = Today.getMonth();
+var Date = Today.getDate();
+// var Hour = Today.getHours();
+// var Minute = Today.getMinutes();
+// var Second = Today.getSeconds();
 var day  = Today.getDay();
 var day_list = ['日', '一', '二', '三', '四', '五', '六'];
-document.querySelector(".current_time").innerHTML = Today.getFullYear()+ "年" + (Today.getMonth()+1) + "月" + Today.getDate() + "日" + ' 星期' + day_list[day] + " " + Today.getHours() + " : " + Today.getMinutes();
+
+var MD = Today.toLocaleDateString([], { month: "2-digit", day: "2-digit" });
+var HM = Today.toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit"});
+var HMS = Today.toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
+
+document.querySelector(".update_date").innerHTML = MD;
+document.querySelector(".update_time").innerHTML = HM;
+// document.querySelector(".current_time").innerHTML = Year + "年" + (Month + 1) + "月" + Date + "日" + " 星期" + day_list[day] + " " + Hour + " : " + Minute + " : " + Second;
+
+function current() {
+    const curr = Year + "年" + (Month + 1) + "月" + Date + "日" + " 星期" + day_list[day] + " " + HMS;
+    document.querySelector(".current_time").innerHTML = curr;
+    setInterval('current()', 1000);
+};
 
 const clickbtn = document.querySelector(".btn");
 clickbtn.addEventListener('click', toBedStatus);
--- Register/script.js
+++ Register/script.js
@@ -2,17 +2,24 @@ var Today = new Date();
 var Year = Today.getFullYear();
 var Month = Today.getMonth();
 var Date = Today.getDate();
-var Hour = Today.getHours();
-var Minute = Today.getMinutes();
+// var Hour = Today.getHours();
+// var Minute = Today.getMinutes();
+// var Second = Today.getSeconds();
 var day  = Today.getDay();
 var day_list = ['日', '一', '二', '三', '四', '五', '六'];
 
-document.querySelector(".update_date").innerHTML = (Month + 1) + "/" + Date;
-document.querySelector(".update_time").innerHTML = Hour + ":" + Minute;
+var MD = Today.toLocaleDateString([], { month: "2-digit", day: "2-digit" });
+var HM = Today.toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit"});
+var HMS = Today.toLocaleTimeString([], { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" });
+
+document.querySelector(".update_date").innerHTML = MD;
+document.querySelector(".update_time").innerHTML = HM;
+// document.querySelector(".current_time").innerHTML = Year + "年" + (Month + 1) + "月" + Date + "日" + " 星期" + day_list[day] + " " + Hour + " : " + Minute + " : " + Second;
 
 function current() {
-    document.querySelector(".current_time").innerHTML = Year + "年" + (Month + 1) + "月" + Date + "日" + ' 星期' + day_list[day] + " " + Hour + " : " + Minute;
-    setTimeout('current()', 60000);
+    const curr = Year + "年" + (Month + 1) + "月" + Date + "日" + " 星期" + day_list[day] + " " + HMS;
+    document.querySelector(".current_time").innerHTML = curr;
+    setInterval('current()', 1000);
 };
 
 const clickbtn = document.querySelector(".btn");
