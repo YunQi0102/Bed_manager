fc6b0b8921235f0d05d8d34143e61b0ac788f5f8
mei970073 <93147911+YunQi0102@users.noreply.github.com>
2023-12-06 01:41:28 +0800

    add patient list

--- Register/Form.php
+++ Register/Form.php
@@ -50,6 +50,8 @@
                 <img src="..\img\LOGO_register.png" width="33px" class="first_img"><img src="..\img\LOGO_register2.png" width="33px" class="second_img"><br><br>登記</a></li>
             <li><a href="https://yunqi0102.000webhostapp.com/FloorSelect/">
                 <img src="..\img\LOGO_bed.png" width="35px" class="first_img" style="left: 20.5px;"><img src="..\img\LOGO_bed2.png" width="35px" class="second_img" style="left: 20.5px;"><br><br>病床</a></li>
+            <li><a href="https://yunqi0102.000webhostapp.com/Patient/index.php">
+                <img src="..\img\LOGO_patient.png" width="35px" class="first_img" style="left: 20.5px;"><img src="..\img\LOGO_patient2.png" width="35px" class="second_img" style="left: 20.5px;"><br><br>名單</a></li>
             <p>最後更新<br><span class="update_date"></span><br><span class="update_time"></span></p>
         </ul>
     </nav>
@@ -89,22 +91,14 @@
                 <input type="number" name="clinic_number" class="insert" readonly=true 
                     value="<?php echo "{$row[4]}"; ?>">
             </td>
-            <td>登記日期/時間：
+            <!--<td>登記日期/時間：-->
                 <!--<input type="datetime" name="register_datetime" class="insert" readonly=true-->
                 <!--    value="<?php echo $current; ?>">-->
-                <span class="insert"><?php echo $current; ?></span>
+            <!--    <span class="insert"><?php echo $current; ?></span>-->
+            <!--</td>-->
             <td>簽證醫師：
                 <input type="text" name="visa_doctor" class="insert" readonly=true 
                     value="<?php echo "{$row[5]}"; ?>">
-            </td>
-        </tr>
-        <tr>
-            <td>連絡電話：
-                <input type="tel" name="telephone_number" class="input" maxlength="10" value="0">
-            </td>
-            <td>預約日期/時間：
-                <input type="date" name="reserve_date" class="input" style="width: 120px;">
-                <!--<input type="time" name="reserve_time" class="input" style="width: 115px;">-->
             </td>
             <td>隔離與否：
                 <input type="radio" name="isolation" id="isolate_yes" value=1>
@@ -114,14 +108,23 @@
             </td>
         </tr>
         <tr>
+            <td>連絡電話：
+                <input type="tel" name="telephone_number" class="input" maxlength="10" value="0">
+            </td>
+            <td>預約日期：
+                <input type="date" name="reserve_date" class="input" style="width: 120px;">
+                <!--<input type="time" name="reserve_time" class="input" style="width: 115px;">-->
+            </td>
+            <td>離院日期：
+                <input type="date" name="discharged_date" class="input" style="width: 110px;">
+            </td>
+        </tr>
+        <tr>
             <td>手機號碼：
                 <input type="tel" name="phone_number" class="input" maxlength="10">
             </td>
             <td>預約住院天數：
                 <input type="number" name="reserve_days" class="input" min="1" value=null;>
-            </td>
-            <td rowspan="2">隔離註記：
-                <input type="text" name="isolation_note" class="input remark" style="width: 290px;">
             </td>
         </tr>
         <tr>
@@ -132,17 +135,6 @@
                     <option>開刀</option>
                     <option>化療</option>
                 </select>
-            </td>
-            <td>離院日期：
-                <input type="date" name="discharged_date" class="input" style="width: 110px;">
-            </td>
-        </tr>
-        <tr>
-            <td>手術住院：
-                <input type="radio" name="operation" id="surgery_yes" value=1>
-                <label for="surgery_yes">是</label>
-                <input type="radio" name="operation" id="surgery_no" value=0>
-                <label for="surgery_no">否</label>
             </td>
             <td>重大傷病：
                 <input type="radio" name="severely_injured" id="severe_yes" value=1>
@@ -158,6 +150,12 @@
             </td>
         </tr>
         <tr>
+            <td>手術住院：
+                <input type="radio" name="operation" id="surgery_yes" value=1>
+                <label for="surgery_yes">是</label>
+                <input type="radio" name="operation" id="surgery_no" value=0>
+                <label for="surgery_no">否</label>
+            </td>
             <td>化療住院：
                 <input type="radio" name="chemotherapy" id="chemotherapy_yes" value=1>
                 <label for="chemotherapy_yes">是</label>
@@ -169,6 +167,11 @@
                 <label for="intensive_care_yes">是</label>
                 <input type="radio" name="ICU" id="intensive_care_no" value=0>
                 <label for="intensive_care_no">否</label>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">隔離註記：
+                <input type="text" name="isolation_note" class="input remark" size="135">
             </td>
         </tr>
         <tr>
